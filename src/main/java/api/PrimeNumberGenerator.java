46f452ddac580ce379e091a785464124c7578d17
burkhart-demos <jburkhartgithub@gmail.com>
2021-05-30 22:05:04 -0400

    Add Javadoc for PrimeNumberGenerator since not already added.

--- src/main/java/api/PrimeNumberGenerator.java
+++ src/main/java/api/PrimeNumberGenerator.java
@@ -5,7 +5,22 @@ import java.util.List;
 public interface PrimeNumberGenerator {
     	
     public static final Integer FIRST_PRIME_NUMBER = 2;
+    
+    /**
+     * Returns all the prime numbers found in the fully inclusive range from [startValue, endValue].
+     * 
+     * @param startValue starting integer of range
+     * @param endValue ending integer of range
+     * @return a list of any prime numbers in range
+     */
     List<Integer> generate(int startValue, int endValue);
+    
+    /**
+     * Returns whether the given value is a prime number.
+     * 
+     * @param value integer to test
+     * @return true if prime, else false
+     */
     boolean isPrime(int value);
 	
 }
